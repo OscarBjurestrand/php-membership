620f3d7609556ccef532977e6c630c056e790703
Oscar Bjurestrand <oscar.bjure98@gmail.com>
2017-11-19 20:49:39 +0100

    Added prepared statements.
    Prepared statements are implemented to add-user.php.

--- code/php/add-user.php
+++ code/php/add-user.php
@@ -39,32 +39,50 @@ elseif (!ctype_alnum($post_username)) {
     echo "<a href='login.php'>Try again</a>";
 }
 else {
-	//create query for username search
-	$query = "SELECT Username FROM users WHERE Username = '$post_username'";
+	/* Prepared statement, stage 1: prepare */
+	if (!($stmt = mysqli_prepare($conn, "SELECT Username FROM users WHERE Username = (?)"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+
+	/* Prepared statement, stage 2: bind and execute */
+	if (!$stmt->bind_param('s', $post_username)) {
+	    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
 
 	//check if username exists or not
-	if ($result = $conn->query($query)) {
+	if ($result = $stmt->execute()) {
 
-	    //check if there already is a row with $post_username
-	    if (mysqli_num_rows($result) != 0) {
-	    	echo "<p>Username already exists.</p>";
+		//if the statement fetches anything but zero, then another user with the same username exists
+		if ($stmt->fetch() != 0) {
+			echo "<p>Username already exists.</p>";
 			echo "<a href='register.php'>Try again</a>";
-	    } 
+			//close statement
+	    	$stmt->close();
+		}
 	    else
 	    {
+	    	/* Prepared statement, stage 1: prepare */
+			if (!($stmt = mysqli_prepare($conn, "INSERT INTO users (`Username`, `Password`) VALUES ((?), (?))"))) {
+		    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+			}
+
+			/* Prepared statement, stage 2: bind and execute */
+			if (!$stmt->bind_param('ss', $post_username, $hashed_post_password)) {
+			    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+			}
+
 	    	//check if the query was successful and insert username and hashed password to database
-			if (mysqli_query($conn, "INSERT INTO users (`Username`, `Password`) VALUES ('$post_username', '$hashed_post_password')")) {
+			if ($stmt->execute()) {
 			 	echo "<p>User was succesfully created</p>";
 			 	echo "<a href='login.php'>Go to login</a>";
 			 } 
 			 else {
-			 	echo "<p>User was not added.</p>";
-			 	echo "Error" . mysqli_error();
-			 	echo "<a href='register.php'>Return</a>";
+			 	echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 			 }
 	    }
-	    //free result memory
-	    $result->close();
+	}
+	else {
+		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 	}
 }
 ?>
