3e1292da082135d8cb26de6910109d263fdb7147
Oscar Bjurestrand <oscar.bjure98@gmail.com>
2017-11-19 16:44:45 +0100

    Check version
    Added check for version 5.0.0 of PHP.

--- code/php/connect.php
+++ code/php/connect.php
@@ -8,9 +8,19 @@ $dbname = 'users'; //database name
 // Create connection
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-// Check connection
-if (mysqli_connect_errno()) {
-    printf("Connect failed: ", mysqli_connect_error());
-    die();
+//check if the server has at least PHP 5.0.0 installed
+if (strnatcmp(phpversion(),'5.0.0') >= 0)
+{
+	 // Check connection
+	if (mysqli_connect_errno()) {
+	    printf("Connect failed: ", mysqli_connect_error());
+	    die();
+	}
+}
+else
+{
+  echo '<h1>Your php version is not supported by php-membership.</h1>';
+  echo "Your php version is: " . phpversion() . ".";
+  echo "You need at least PHP 5.0.0 in order to run php-membership.";
 }
 ?>
